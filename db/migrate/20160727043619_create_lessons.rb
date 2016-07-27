1d38dbef2fad9b832fd096356a80ede18dbbbded
melissa brown <melissabrown851@gmail>
2016-07-27 06:09:29 +0000

    added lessons to the course page

---
+++ db/migrate/20160727043619_create_lessons.rb
@@ -0,0 +1,12 @@
+class CreateLessons < ActiveRecord::Migration
+  def change
+    create_table :lessons do |t|
+      t.string :title
+      t.string :subtitle
+
+      t.integer :section_id
+
+      t.timestamps
+    end
+  end
+end
