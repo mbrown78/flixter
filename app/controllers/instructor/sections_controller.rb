b599b4e96d6ae1039468b24e22b63836c96aedef
melissa brown <melissabrown851@gmail>
2016-08-10 06:34:35 +0000

    now instructors can reorder sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,8 @@
 class Instructor::SectionsController < ApplicationController
     before_action :authenticate_user!
-    before_action :require_authorized_for_current_course
+    before_action :require_authorized_for_current_course, only: [:new, :create]
+    before_action :require_authorized_for_current_section, only: [:update]
+    
     
     def new
         @section = Section.new
@@ -11,6 +13,11 @@ class Instructor::SectionsController < ApplicationController
         redirect_to instructor_course_path(current_course)
     end
     
+    def update
+        current_section.update_attributes(section_params)
+        render text: 'updated!'
+    end    
+    
     private
     
     def require_authorized_for_current_course
@@ -19,13 +26,26 @@ class Instructor::SectionsController < ApplicationController
         end
     end
     
+    def require_authorized_for_current_section
+        if current_course.user != current_user
+            render text: "Unauthorized", status: :unauthorized
+        end    
+    end        
+        
+    
+    
     helper_method :current_course
     def current_course
         @current_course ||= Course.find(params[:course_id])
     end
-
+    
+    helper_method :current_section
+    def current_section
+        @current_section ||= Section.find(params[:section_id])
+    end    
+         
     def section_params
-        params.require(:section).permit(:title)
+        params.require(:section).permit(:title, :row_order_position)
     end
     
 end
