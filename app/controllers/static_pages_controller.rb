afbc6eb6ba48851bd8cce23462651b9ab898665f
melissa brown <melissabrown851@gmail>
2016-08-12 09:36:51 +0000

    added links to footer pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,15 @@ class StaticPagesController < ApplicationController
     before_action :authenticate_user!
     
     def index 
+    end  
+    
+    def privacy
+    end    
+    
+    def careers
+    end
+    
+    def team
     end    
     
 end
